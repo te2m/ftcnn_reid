d2fca3244966f3f73db34b10d196026a34c02c3a
te2m <matsukawa@inf.kyushu-u.ac.jp>
2018-06-14 17:54:31 +0900

    <comment>

--- extract_features.py
+++ extract_features.py
@@ -1,162 +1,84 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# CNN feature extraction
-
 import numpy as np
 import sys, os, os.path, caffe, glob
 import matplotlib.pyplot as plt
 
-GPU_ID = 0
 caffe.set_mode_gpu()
-caffe.set_device(GPU_ID)
 
-MODELDIR = '../Caffe_model/'
-DATADIR_ROOT = '../Data/'
-SAVEDIR_ROOT = '../Data_feature/'
+MODELDIR = 'Caffe_model/'               # directly of trained model
+DATADIR_ROOT = '/mnt/HDD/Data/'         # root directly of data
+SAVEDIR_ROOT = '/mnt/HDD/Data_feature/' # root directly to save extracted features
 
-####################### Patemeters (below) ##############################################
-DBNAME='Duke'#　database to apply 'VIPeR' or 'CUHK' or 'PRID' or 'GRID'
-alpha = 0.5       # contrubution of combination attributes (0<= alpha <= 1), Higher value corresponds higher contributtion.
-G = 7 	          # group number (fixed to 7)
-r = 7 		  # combination number of attribute groups to combine  r \in { 2,3,4,5,6,7}
-Nmin = 5          # threshold for the minimum image number of combination attributes.
-subset = 1        # subset number in 7Cr attribute groups (it needs when r!=7)
+DBNAME='Market'  # database to extract CNN features
+LAYERS=['fc6']  # layer name to extract CNN features
+				# e.g., LAYERS=['pool1', 'pool2', 'conv3', 'conv4', 'pool5', 'fc6', 'fc7']
 
-ITES = ['50000']  #　iteration number of CNN fintuning to use feature extraction
-		  #　ITES = ['5000', '10000', '50000', '100000']
+SAVEDIR= SAVEDIR_ROOT + DBNAME + '/FTAlex/'
 
-LAYERS=['fc6']    #　layer of CNN features
-		  #　LAYERS=['pool1', 'pool2', 'conv3', 'conv4', 'pool5', 'fc6', 'fc7']
+## load CNN
+if DBNAME == 'VIPeR' or DBNAME == 'CUHK' or DBNAME == 'PRID' or DBNAME == 'GRID':
+	PRETRAINED = MODELDIR +  'Alexnet_PETA_except_' + DBNAME + '_comb_r7+multi_alpha0.5_train_iter_50000.caffemodel'
+else:
+	PRETRAINED = MODELDIR +  'Alexnet_PETA_all_comb_r7+multi_alpha0.5_train_iter_50000.caffemodel'
 
-####################### Patemeters (upto here) #####################################################
+MODEL_FILE = MODELDIR + 'Alexnet_deploy.prototxt'
 
+# Use ImageNet means for all pixels
+meanval = np.array([104.0069873, 116.66876762, 122.67891434])
+net = caffe.Classifier(MODEL_FILE, PRETRAINED, mean=meanval, channel_swap=(2,1,0), raw_scale=255, image_dims=(227,227))
+
+## setup of database
 if DBNAME == 'VIPeR':
 	DATADIR = DATADIR_ROOT + 'VIPeR/'
 	CAMS=['cam_a/', 'cam_b/']
+	EXT = '.bmp'
+
 elif DBNAME == 'CUHK':
 	DATADIR= DATADIR_ROOT +'CUHK01/'
+	CAMS=['campus']
+	EXT = '.png'
+
 elif DBNAME == 'PRID':
 	DATADIR= DATADIR_ROOT +'prid_450s/'
 	CAMS=['cam_a/', 'cam_b/']
+	EXT = '.png'
+
 elif DBNAME == 'GRID':
 	DATADIR = DATADIR_ROOT + 'GRID/'
 	CAMS=['gallery/', 'probe/']
-
-elif DBNAME =='i-LID':
-	DATADIR = DATADIR_ROOT + 'i-LIDS-VID/'
-	CAMS=['images/cam1', 'images/cam2', 'sequences/cam1', 'sequences/cam2']
-
-elif DBNAME == 'CUHK03':
-	DATADIR = DATADIR_ROOT + 'CUHK03/'
-	CAMS=['labeled/P1/cam1', 'labeled/P1/cam2', 'labeled/P2/cam1', 'labeled/P2/cam2',
-	      'labeled/P3/cam1', 'labeled/P3/cam2',
-	      'labeled/P4/cam1', 'labeled/P4/cam2', 'labeled/P5/cam1', 'labeled/P5/cam2',
-	      'detected/P1/cam1', 'detected/P1/cam2', 'detected/P2/cam1', 'detected/P2/cam2',
-	      'detected/P3/cam1', 'detected/P3/cam2',
-	      'detected/P4/cam1', 'detected/P4/cam2', 'detected/P5/cam1', 'detected/P5/cam2']
+	EXT = '.jpeg'
 
 elif DBNAME == 'Market':
 	DATADIR = DATADIR_ROOT + 'Market-1501-v15.09.15/'
 	CAMS=['bounding_box_test' , 'bounding_box_train', 'gt_bbox', 'gt_query', 'query']
+	EXT = '.jpg'
 
 elif DBNAME == 'Duke':
 	DATADIR = DATADIR_ROOT + 'DukeMTMC-reID/'
 	CAMS=['bounding_box_test' , 'bounding_box_train', 'query']
+	EXT = '.jpg'
 
-print len(CAMS)
+###################  CNN feature extraction and save  ########################################
+# create save directory
+for c in range(len(CAMS)):
+	for layer in LAYERS:
+		dirname = SAVEDIR + layer + '/' + CAMS[c]
+		if os.path.exists(dirname) == False:
+			os.makedirs(dirname)
 
+# extract features and save
+for c in range(len(CAMS)):
+	list = glob.glob( DATADIR+CAMS[c]+'/*'+EXT)
+	print DATADIR+CAMS[c]
+	print list
+	for file in list:
+		print file
+		image = caffe.io.load_image(file)
+		file = file[len(DATADIR+CAMS[c]):len(file)]
 
-# CNN name
-exceptDB = DBNAME
-EXPNAME =  "except_" + "_r"+str(r)+"Nmin"+str(Nmin)
-if r != 7:
-	EXPNAME = EXPNAME + "subset" + str(subset)
-EXPNAME = EXPNAME + "+multi_"
-C_EXPNAME = EXPNAME + 'alpha' + str(alpha)
-EXP="FTAlex_" + str(G+1) + "Task"
-
-
-for ITE in ITES:
-	## load CNN
-	MODEL_FILE = MODELDIR + EXP + '/Prototxt/Instance/Alexnet_' + C_EXPNAME + '_deploy.prototxt'
-	PRETRAINED = MODELDIR + EXP + '/Snapshot/Alexnet_' + C_EXPNAME + '_train_iter_' + ITE + '.caffemodel'
-
-	# Use ImageNet means for all pixels [104.00698793  116.66876762  122.67891434]
-	meanval = np.array([104.0069873, 116.66876762, 122.67891434])
-	net = caffe.Classifier(MODEL_FILE, PRETRAINED, mean=meanval, channel_swap=(2,1,0), raw_scale=255, image_dims=(227,227))
-
-	INDEX = 4
-	## CNN feature extraction and save
-	if DBNAME == 'CUHK':
-		SAVEDIR= SAVEDIR_ROOT + 'CUHK01/' + EXP + '/' + C_EXPNAME + 'ite' + ITE + '/'
-	elif DBNAME == 'PRID':
-		SAVEDIR= SAVEDIR_ROOT + 'prid_450s/' + EXP +  '/' + C_EXPNAME + 'ite' + ITE +'/'
-	else:
-		SAVEDIR= SAVEDIR_ROOT + DBNAME + '/'  + EXP + '/' + C_EXPNAME + 'ite' + ITE + '/'
-
-	if DBNAME == 'Market' or DBNAME == 'Duke':
-		# create save directory
-		for c in range(len(CAMS)):
-			for layer in LAYERS:
-				dirname = SAVEDIR + layer + '/' + CAMS[c]
-				if os.path.exists(dirname) == False:
-					os.makedirs(dirname)
-
-		for c in range(len(CAMS)):
-			list = glob.glob( DATADIR+CAMS[c]+'/*.jpg')
-			print DATADIR+CAMS[c]
-			print list
-			for file in list:
-				print file
-				image = caffe.io.load_image(file)
-				#plt.imshow(image)
-
-				file = file[len(DATADIR+CAMS[c]):len(file)]
-
-				for layer in LAYERS:
-
-					filesave = SAVEDIR + layer + '/' + CAMS[c] + file + '.txt'
-					print filesave
-					f = open(filesave, 'w')
-					net.predict([ image ])
-					feat = net.blobs[layer].data[INDEX].flatten().tolist()
-					print len(feat)
-
-					for d in range(len(feat)):
-						f.writelines(str(feat[d])+" ")
-
-					f.close()
-
-
-	if DBNAME == 'CUHK03':
-		# create save directory
-		for c in range(20):
-			for layer in LAYERS:
-				dirname = SAVEDIR + layer + '/' + CAMS[c]
-				if os.path.exists(dirname) == False:
-					os.makedirs(dirname)
-
-		for c in range(20):
-			list = glob.glob( DATADIR+CAMS[c]+'/*.png')
-			print DATADIR+CAMS[c]
-			print list
-			for file in list:
-				print file
-				image = caffe.io.load_image(file)
-				#plt.imshow(image)
-
-				file = file[len(DATADIR+CAMS[c]):len(file)]
-
-				for layer in LAYERS:
-
-					filesave = SAVEDIR + layer + '/' + CAMS[c] + file + '.txt'
-					print filesave
-					f = open(filesave, 'w')
-					net.predict([ image ])
-					feat = net.blobs[layer].data[INDEX].flatten().tolist()
-					print len(feat)
-
-					for d in range(len(feat)):
-						f.writelines(str(feat[d])+" ")
-
-					f.close()
+		for layer in LAYERS:
+			filesave = SAVEDIR + layer + '/' + CAMS[c] + file + '.csv'
+			print filesave
+			net.predict([ image ])
+			feat = net.blobs[layer].data[4].flatten()
+			print feat.size
+			np.savetxt(filesave, feat, delimiter=',')
